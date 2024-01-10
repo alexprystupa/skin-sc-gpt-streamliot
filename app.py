c536566b20ed7ea77a971111d1debcc468152291
alexprystupa <alex.prystupa@gmail.com>
2024-01-10 11:20:59 -0500

    version 1.0.1

--- app.py
+++ app.py
@@ -4,17 +4,14 @@ import streamlit as st
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import Qdrant
 
-from helperfuncs.qdrant_functions import load_qdrant_client
-from helperfuncs.qdrant_functions import load_single_db
-
-from helperfuncs.pdf_chat_functions import get_conversation_chain
-
-from helperfuncs.recommendation_functions import get_best_paper_and_summary
-
 from helperfuncs.aws_functions import get_aws_S3_client
-from helperfuncs.aws_functions import read_S3_meta_data
 from helperfuncs.aws_functions import get_s3_first_img_path_all_papers
 from helperfuncs.aws_functions import get_s3_img_paths_recommended_paper
+from helperfuncs.aws_functions import read_S3_meta_data
+from helperfuncs.pdf_chat_functions import get_conversation_chain
+from helperfuncs.qdrant_functions import load_qdrant_client
+from helperfuncs.qdrant_functions import load_single_db
+from helperfuncs.recommendation_functions import get_best_paper_and_summary
 
 # Load Qdrant Client
 client = load_qdrant_client(url=st.secrets["QDRANT_HOST"], api_key=st.secrets["QDRANT_API_KEY"])
@@ -33,12 +30,25 @@ def set_chat_session_state(prompt):
     st.session_state["prompt"] = prompt
 
 
+@st.cache_data
+def get_list_first_page_all_papers():
+    s3_img_paths = get_s3_first_img_path_all_papers(s3_client=s3_client, bucket_name="sc-pdf-recommendation-bucket")
+
+    return [fs.open(s3_img_path, mode='rb').read() for s3_img_path in s3_img_paths]
+
+    
+@st.cache_data
+def get_list_recommended_paper_imgs(pmid):
+
+    s3_img_paths = get_s3_img_paths_recommended_paper(s3_client=s3_client, bucket_name="sc-pdf-recommendation-bucket",
+                                                      pmid=str(pmid))
+
+    return [fs.open(s3_img_path, mode='rb').read() for s3_img_path in s3_img_paths]
+
+
 def main():
 
     # Set Session States
-    if "chat_button" not in st.session_state:
-        st.session_state["chat_button"] = False
-
     if "prompt" not in st.session_state:
         st.session_state["prompt"] = None
 
@@ -55,12 +65,10 @@ def main():
             st.sidebar.title(st.session_state['prompt_response_dict']['Title'])
             st.write("-----")
 
-            s3_img_paths = get_s3_img_paths_recommended_paper(s3_client=s3_client, bucket_name="sc-pdf-recommendation-bucket",
-                                               pmid=str(st.session_state['prompt_response_dict']['PMID']))
+            paper_images = get_list_recommended_paper_imgs(st.session_state['prompt_response_dict']['PMID'])
 
-            for i, s3_img_path in enumerate(s3_img_paths):
-                st.header(f"Page {i+1}")
-                st.image(fs.open(s3_img_path, mode='rb').read())
+            for paper_img in paper_images:
+                st.image(paper_img)
 
     else:
         st.header("Single Cell PDF Recommendation System :male-scientist:")
@@ -73,11 +81,11 @@ def main():
             st.sidebar.title("Single Cell Skin Sequencing Papers")
             st.write("-----")
 
-            s3_first_img_paths = get_s3_first_img_path_all_papers(s3_client=s3_client, bucket_name="sc-pdf-recommendation-bucket")
+            first_page_images = get_list_first_page_all_papers()
 
-            for i, s3_first_img_path in enumerate(s3_first_img_paths):
+            for i, first_page in enumerate(first_page_images):
                 st.header(f"Paper {i + 1}")
-                st.image(fs.open(s3_first_img_path, mode='rb').read())
+                st.image(first_page)
 
     # Prompt has been asked by the user showing recommended paper
     if prompt and not st.session_state["prompt"]:
@@ -95,14 +103,12 @@ def main():
             st.sidebar.title(prompt_response_dict["Title"])
             st.write("-----")
 
-            st.button("Chat with pdf", key="chat_button", on_click=set_chat_session_state, args=(prompt,))
+            st.button("Chat with pdf", on_click=set_chat_session_state, args=(prompt,))
 
-            s3_img_paths = get_s3_img_paths_recommended_paper(s3_client=s3_client, bucket_name="sc-pdf-recommendation-bucket",
-                                               pmid=str(st.session_state['prompt_response_dict']['PMID']))
+            paper_images = get_list_recommended_paper_imgs(st.session_state['prompt_response_dict']['PMID'])
 
-            for i, s3_img_path in enumerate(s3_img_paths):
-                st.header(f"Page {i + 1}")
-                st.image(fs.open(s3_img_path, mode='rb').read())
+            for paper_img in paper_images:
+                st.image(paper_img)
 
     # User has chosen to ask questions to the paper
     if prompt and st.session_state["prompt"]:
